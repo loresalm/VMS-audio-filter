bf62edb2d9a6b97a2c74c33d27514df020a4baae
loresalm <salmina.lorenzo@outlook.com>
2025-03-18 23:02:31 +0100

    adding led feedback to GUI

--- gainPlugin/GuiTest/GUI_test/Source/PluginEditor.h
+++ gainPlugin/GuiTest/GUI_test/Source/PluginEditor.h
@@ -26,8 +26,10 @@ public:
         dialColour = juce::Colour::fromString ("FF1E90FF"); // Orange for the dial
         // Set the hex color for the text box text (e.g., #FFA500 for orange)
         textColour = juce::Colour::fromString ("FF2a362f");
+        onColour = juce::Colour::fromString ("FF347c87");
+        offColour = juce::Colour::fromString ("FFe35807");
     }
-    /*
+    
     void drawLabel(juce::Graphics& g, juce::Label& label) override
     {
         // Override the label drawing to use the custom text color
@@ -35,11 +37,12 @@ public:
         g.setFont(label.getFont());
         g.drawText(label.getText(), label.getLocalBounds(), juce::Justification::centred, false);
     }
-     */
+     
     void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
                               const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider) override
     {
-        float diameter = juce::jmin(width, height);
+        float s = 1.0f;
+        float diameter = (juce::jmin(width, height))*0.9f;
         float r = diameter/2.;
         float cX = x + width/2.;
         float cY = y + height/2.;
@@ -49,28 +52,49 @@ public:
         float circleRadius = r * 0.1f; // You can adjust this size as needed
         float circleDistance = r * 0.9f; // Distance from center to the circle
         
-
+        
         float rX = cX - r;
         float rY = cY - r;
         /*
+        // plugin debug
         juce::Rectangle<float> dialArea (rX, rY, diameter, diameter);
         g.setColour(dialColour);
         g.drawRect(dialArea);
          */
+        
+        // DIALS BOUNDS
         float diameter_int = diameter - diameter/2.5;
         float r_int = diameter_int/2;
         float rX_int = cX - r_int;
         float rY_int = cY - r_int;
-        juce::Rectangle<float> dialBounds (x, y, width, height);
+        
         juce::Rectangle<float> dialArea_ext (rX, rY, diameter, diameter);
         juce::Rectangle<float> dialArea_int (rX_int, rY_int, diameter_int, diameter_int);
-        //juce::Rectangle<int> intdialcenter = dialcenter.toNearestInt();
-        //g.setColour(textColour);
-        //g.drawRect(dialBounds,  2.0f);
         g.setColour(textColour);
-        g.drawEllipse(dialArea_ext,  2.0f);
-        g.drawEllipse(dialArea_int,  2.0f);
-        //g.fillEllipse(dialArea);
+        g.drawEllipse(dialArea_ext,  s);
+        g.drawEllipse(dialArea_int,  s);
+        //g.fillEllipse(dialArea)
+        /*
+        // plugin debug
+        juce::Rectangle<float> dialBounds (x, y, width, height);
+        juce::Rectangle<int> intdialcenter = dialcenter.toNearestInt();
+        g.setColour(textColour);
+        g.drawRect(dialBounds,  2.0f);
+        */
+
+        
+        // LED FEEDBACK
+        float diameter_led = diameter/7.0f;
+        float r_led = diameter_led/2;
+        float rX_led = cX - r_led;
+        float rY_led = cY - r_led;
+        juce::Rectangle<float> ledArea (rX_led, rY_led, diameter_led, diameter_led);
+        if (sliderPos <= 0.5f)
+        {g.setColour(onColour);}
+        else
+        {g.setColour(offColour);}
+        g.drawEllipse(ledArea,  2.0f);
+        
         
         
         // DOTS
@@ -85,11 +109,13 @@ public:
                              circleRadius * 2.0f, circleRadius * 2.0f);
         for (int i = 0; i <= numDots; ++i)
         {
-            g.setColour(knobColour); // Use knobColour for the dots
-            g.fillPath(dialTicks, juce::AffineTransform::rotation(dotAngle).translated(cX,cY));
-            
+            g.setColour(textColour); // Use knobColour for the dots
+            if (dotAngle < angle){
+                g.fillPath(dialTicks, juce::AffineTransform::rotation(dotAngle).translated(cX,cY));
+            }else{
+                g.strokePath(dialTicks, juce::PathStrokeType(s), juce::AffineTransform::rotation(dotAngle).translated(cX, cY));
+            }
             dotAngle = dotAngle + dotStep;
-            
         }
         
         //TICK
@@ -121,6 +147,8 @@ private:
     juce::Colour textColour; // Custom text color
     juce::Colour knobColour; // Color of the knob
     juce::Colour dialColour; // Color of the dial
+    juce::Colour onColour; // Color of the led when on
+    juce::Colour offColour; // Color of the led when off
 };
 
 
